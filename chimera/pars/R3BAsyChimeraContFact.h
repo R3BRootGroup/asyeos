7434936ad60dcb43aa966cb55cdfaa30a206d00c
edefilippo <defilippo@ct.infn.it>
2023-03-16 16:35:50 +0100

    added CHIMERA and related sim data
    Added CHIMERA and related sim data
    
    added chimera data, sim and pars
    
    added chimera data, sim and pars
    
    before clang
    
    Clang-format
    
    new clang-format
    
    new clang-format
    
    new clang-format
    
    revised clang-format
    
    revised clang-format
    
    revised clang-format
    
    revised clang-format
    
    revised clang-format
    
    revised clang-format
    
    stable release
    
    added CHIMERA and related sim data
    
    added chimera data, sim and pars
    
    added chimera data, sim and pars
    
    Clang-format
    
    new clang-format
    
    revised clang-format
    
    tested and stable
    
    tested and stable
    
    new author
    
    added CHIMERA and related sim data
    
    Added CHIMERA and related sim data
    
    added chimera data, sim and pars
    
    added chimera data, sim and pars
    
    before clang
    
    Clang-format
    
    new clang-format
    
    clangtidy script: init

---
+++ chimera/pars/R3BAsyChimeraContFact.h
@@ -0,0 +1,27 @@
+// -----------------------------------------------------------------------
+//								    ------
+//  Factory for the parameter containers in libR3BAsyChimera        ------
+//        created 27/03/2023 by E. De Filippo and P. Russotto	    ------
+// -----------------------------------------------------------------------
+
+#ifndef R3BAsyChimeraContFact_H
+#define R3BAsyChimeraContFact_H 1
+
+#include "FairContFact.h"
+
+class FairContainer;
+
+
+class R3BAsyChimeraContFact : public FairContFact
+{
+  private:
+    void setAllContainers();
+
+  public:
+    R3BAsyChimeraContFact();
+    ~R3BAsyChimeraContFact() {}
+    FairParSet* createContainer(FairContainer*);
+    ClassDef(R3BAsyChimeraContFact, 0) // Factory for all Chimera parameter containers
+};
+
+#endif /* R3BAsyChimeraContFact_H */
