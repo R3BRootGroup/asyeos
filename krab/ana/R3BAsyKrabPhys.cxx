b21cb18b86bd057babf711684f14e0b1cff1fe6e
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2025-03-06 23:27:10 +0100

    TofD:Online update
    seset of some online histos fixed

--- krab/ana/R3BAsyKrabPhys.cxx
+++ krab/ana/R3BAsyKrabPhys.cxx
@@ -86,11 +86,12 @@ InitStatus R3BAsyKrabPhys::Init()
     LOG(info) << "R3BAsyKrabDoArray::Init DONE";
 
     FairRunOnline* run = FairRunOnline::Instance();
-    // for online server  run->GetHttpServer()->Register("", this);
+    // for online server  
+    run->GetHttpServer()->Register("", this);
 
     // Register command to reset histograms
-    // for online server  run->GetHttpServer()->RegisterCommand("Reset_Krab",
-    // Form("/Objects/%s/->Reset_Histo()", GetName()));
+    // for online server  
+    run->GetHttpServer()->RegisterCommand("Reset_Krab_Phys", Form("/Objects/%s/->Reset_Histo()", GetName()));
 
     // --- ------------------------------------- --- //
     // --- get access to mapped data of the TofW --- //
